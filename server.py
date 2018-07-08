750c84aad1cf2e872e0c41185903e33e3c1903d1
Ken <guitarsrockalot@gmail.com>
2018-07-08 05:21:44 +0000

    fixed exception thrown when client session ends

--- server.py
+++ server.py
@@ -6,16 +6,17 @@ def main():
         s = socket.socket()
         s.bind((host, port))
 
-        s.listen(1)
-        c, addr = s.accept()
+	while True:
+        	s.listen(1)
+        	c, addr = s.accept()
 
-        while True:
-                data = c.recv(1024)
-                if not data:
-                        break
-                data = str(data).upper()
-                c.send(data)
-        c.close()
+        	while True:
+                	data = c.recv(1024)
+                	if not data:
+                        	break
+                	data = str(data).upper()
+                	c.send(data)
+        	c.close()
 
 if __name__ == '__main__':
         main()
